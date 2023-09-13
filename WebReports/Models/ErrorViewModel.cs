fe9920c9b8488d1f24327089dfd55c668edb9aef
Vinod Rao Vutpala <vinod.vutpala@snp.com>
2023-09-13 05:56:53 -0400

    Initial version Asp.Net Core MVC Web App with local Identiy scaffolded.

---
+++ WebReports/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebReports.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
