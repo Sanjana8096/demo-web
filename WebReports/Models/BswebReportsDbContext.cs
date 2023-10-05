39c9f417229f544089037491e0a0996d6108383b
devappa.c@snp.com <devappa.c@snp.com>
2023-10-05 14:42:44 +0530

    database updated to azure sql database from localdb

--- WebReports/Models/BswebReportsDbContext.cs
+++ WebReports/Models/BswebReportsDbContext.cs
@@ -34,8 +34,7 @@ public partial class BSWebReportsDbContext : DbContext
     public virtual DbSet<ClientUser> ClientUsers { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=BSWebReports;Trusted_Connection=True;");
+        => optionsBuilder.UseSqlServer("data source=vut-cmp-dev-sqlserver.database.windows.net;initial catalog=BSWebReports;persist security info=True;user id=snpadmin;password=snp@1234;MultipleActiveResultSets=True;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
